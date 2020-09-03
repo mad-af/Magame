00a358da69a15c8e5d43b06000d0e63f5869bb6c
Mochamad Aufa <mochamadaufa7@gmail.com>
2020-09-03 23:01:01 +0700

    add components-element-particles

--- src/components/element/Particles/component.js
+++ src/components/element/Particles/component.js
@@ -1,22 +1,62 @@
 import React, {Component} from "react";
 import Particle from "react-particles-js";
-import { withStyles } from '@material-ui/styles';
 
-const isStyles = ({
-  screen:{
-    overflow: 'hidden',
-  }
-});
 
 class Particles extends Component {
+
+  isParams = {
+    particles: {
+      "number": {
+        "value": 150,
+        "density": {
+          "enable": true,
+          "value_area": 900
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "opacity": 0.02
+      },
+      "move": {
+        "enable": true,
+        "speed": .5,
+        "out_mode": "out"
+      },
+      "size": {
+        "value": 1,
+        "random": true
+      },
+      "opacity": {
+        "anim": {
+          "enable": true,
+          "speed": 3,
+          "opacity_min": 0.05
+        }
+      }
+    },
+    interactivity: {
+      "events": {
+        "onclick": {
+          "enable": true,
+          "mode": "repulse"
+        }
+      },
+      "modes": {
+        "repulse": {
+          "distance": 100
+        }
+      }
+    },
+    retina_detect: true
+  };
+
   render() {
-    const { classes } = this.props;
       return (
           <div>
-              <Particle className={ classes.screen } />
+              <Particle  params={ this.isParams } />
           </div>
       );
   };
 };
 
-export default withStyles(isStyles)(Particles)
+export default (Particles)
